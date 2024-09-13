52bc22432f42e24848f1ba2e0183497fed9c0a8e
rayoneitalo <rayoneitalo@gmail.com>
2024-09-13 03:25:57 +0000

    initial project

---
+++ server/src/database/seed.ts
@@ -0,0 +1,42 @@
+import { client, db } from "."
+import { goalCompletions, goals } from "./schema"
+import dayjs from "dayjs"
+
+async function seed() {
+    await db.delete(goalCompletions)
+    await db.delete(goals)
+
+    const creatingGoals = await db.insert(goals).values([
+        {
+            title: 'Acordar cedo',
+            desiredWeeklyFrequency: 5
+        },
+        {
+            title: 'Me exercitar',
+            desiredWeeklyFrequency: 3
+        },
+        {
+            title: 'Limpar a casa',
+            desiredWeeklyFrequency: 2
+        }
+    ]).returning()
+
+    const startOfWeek = dayjs().startOf('week')
+
+
+    await db.insert(goalCompletions).values([
+        {
+            goalId: creatingGoals[0].id,
+            createdAt: startOfWeek.toDate()
+        },
+
+        {
+            goalId: creatingGoals[1].id,
+            createdAt: startOfWeek.add(1, 'day').toDate()
+        }
+    ])
+}
+
+seed().finally(() => {
+    client.end()
+})
